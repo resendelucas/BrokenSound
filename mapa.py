77fbfae3040ddb7b100b17556537f5280679a07a
GusMedeiros <96637520+GusMedeiros@users.noreply.github.com>
2022-06-10 09:39:23 -0300

    Tornando __init__ do Mapa coeso

--- mapa.py
+++ mapa.py
@@ -1,16 +1,12 @@
 from PPlayTeste.gameimage import *
 
-class Mapa():
+
+class Mapa:
     def __init__(self, janela):
         self.janela = janela
-        self.get_elements()
-        self.background = None
-        self.floor = None
-
-    def get_elements(self):
         self.background = GameImage("Assets/imagens/sky.png")
         self.floor = GameImage("Assets/imagens/floor.png")
-        self.floor.set_position(0,608)
+        self.floor.set_position(0, 608)
 
     def draw_elements(self):
         self.background.draw()
